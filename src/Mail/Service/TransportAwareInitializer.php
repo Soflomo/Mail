58c8bc1f54bb359efb93f9e3490b31ca9569f0af
Jurian Sluiman <jurian@soflomo.com>
2014-05-07 11:32:03 +0200

    Update license to 2012-2014 for Soflomo

--- src/Mail/Service/TransportAwareInitializer.php
+++ src/Mail/Service/TransportAwareInitializer.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Copyright (c) 2012 Jurian Sluiman.
+ * Copyright (c) 2012-2014 Soflomo
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -32,11 +32,9 @@
  * ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
  * POSSIBILITY OF SUCH DAMAGE.
  *
- * @package     Soflomo\Mail
  * @author      Jurian Sluiman <jurian@soflomo.com>
- * @copyright   2012 Jurian Sluiman.
+ * @copyright   2012-2014 Soflomo
  * @license     http://www.opensource.org/licenses/bsd-license.php  BSD License
- * @link        http://soflomo.com
  */
 
 namespace Soflomo\Mail\Service;
