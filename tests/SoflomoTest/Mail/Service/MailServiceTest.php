347c599fc7d815fd3f56824a394bd83f2fe3e20c
Jurian Sluiman <jurian@soflomo.com>
2014-07-17 16:43:51 +0200

    Enable tests for Soflomo\Mail

---
+++ tests/SoflomoTest/Mail/Service/MailServiceTest.php
@@ -0,0 +1,65 @@
+<?php
+/**
+ * Copyright (c) 2012-2014 Soflomo.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ *   * Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *
+ *   * Redistributions in binary form must reproduce the above copyright
+ *     notice, this list of conditions and the following disclaimer in
+ *     the documentation and/or other materials provided with the
+ *     distribution.
+ *
+ *   * Neither the names of the copyright holders nor the names of the
+ *     contributors may be used to endorse or promote products derived
+ *     from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ * COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ * ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ * @author      Jurian Sluiman <jurian@soflomo.com>
+ * @copyright   2012-2014 Soflomo.
+ * @license     http://www.opensource.org/licenses/bsd-license.php  BSD License
+ */
+
+namespace SoflomoTest\Mail\Service;
+
+use PHPUnit_Framework_TestCase as TestCase;
+use Soflomo\Mail\Service\MailService;
+use SoflomoTest\Mail\Util\ServiceManagerFactory;
+use SoflomoTest\Mail\Util\TestTransport;
+use Zend\View\Renderer\PhpRenderer;
+
+class MailServiceTest extends TestCase
+{
+    protected $renderer;
+    protected $transport;
+
+    public function setUp()
+    {
+        $this->renderer  = new PhpRenderer;
+        $this->transport = new TestTransport;
+    }
+
+    public function testCanCreateInstance()
+    {
+        $service = new MailService($this->transport, $this->renderer);
+
+        $this->assertInstanceOf('Soflomo\Mail\Service\MailService', $service);
+    }
+}
